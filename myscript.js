501ce5d383d15a518d7ffbb19cd9e6348b755d96
yamakuza <rednaxreda@gmail.com>
2022-05-17 23:10:00 +0100

    adding the prompt function

--- myscript.js
+++ myscript.js
@@ -1,46 +1,75 @@
 let choices = ["rock" , "paper" , "sccissors"];
-const playerSelection = "scissors";
-const computerSelection = computerplay();
+const computerSelection = computerPlay();
+const playerSelection = playerPlay();
 
 
-
-function computerplay() {
-    
-    random = Math.floor(Math.random() * choices.length);
-    if (random == 0){
-        return choices[0];
-    }
-
-    else if (random == 1){
-        return choices[1];
-    }
-    
-    else if (random == 2){
-        return choices[2];
-    }
-
+function playerPlay() {
+   console.log ("Do you choose 'Rock' (1), 'Paper' (2), or 'Scissors' (3)?");
+   let playerChoice = prompt("Do you choose 'Rock' (1), 'Paper' (2), or 'Scissors' (3)?");
+   if (playerChoice.toLowerCase() === "rock" || parseInt(playerChoice) === 1) {
+      playerChoice = "Rock";
+   } else if (playerChoice.toLowerCase() === "paper" || parseInt(playerChoice) === 2) {
+      playerChoice = "Paper";
+   }
+   else if (playerChoice.toLowerCase() === "scissors" || parseInt(playerChoice) === 3) {
+      playerChoice = "Scissors";
+   }
+   else if (playerChoice.toLowerCase() !== "rock" || "scissors" || "paper" || parseInt(playerChoice) !== 1 || 2 || 3) {
+      alert("Please try to enter your value again :)")
+      playerSelection();
+   }
+   else {
+      console.log("Not sure what's going on, hold on to yer butts ;)")
+   }
+   return playerChoice;
 }
-//console.log(computerplay())
 
-function playGround(playerSelection,computerSelection){
+function computerPlay() {
+
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];
     
-    if (computerSelection == "rock" && playerSelection == "scissors"){
-        return "computer won";
-    }
-    else if (computerSelection === "rock" && playerSelection == "paper"){
-        return "player won";
-    }
-    else if (computerSelection === "scissors" && playerSelection == "rock"){
-        return "player won";
-    }
-    else if (computerSelection === "scissors" && playerSelection == "paper"){
-        return "computer won";
-    }
-    else if (computerSelection === "paper" && playerSelection == "rock"){
-        return "computer won";
-    }
-    else if (computerSelection === "paper" && playerSelection == "scissors"){
-        return "player won";
-    }
 }
-console.log(playGround())
+
+
+
+
+
+
+function playRound(playerSelection,computerSelection){
+    if (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
+        console.log("It's a draw!");
+        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+     } else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissors") {
+        console.log("Rock beats scissors! You lose :(");
+        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+     } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "rock") {
+        console.log("Rock beats scissors! You win :D");
+        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+     } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper") {
+        console.log("Scissors beats paper! You lose :(");
+        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+     } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "scissors") {
+        console.log("Scissors beats paper! You win :D");
+        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+     } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock") {
+        console.log("Paper beats rock! You lose :(");
+        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+     } else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "paper") {
+        console.log("Paper beats rock! You win :D");
+        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+     } else {
+        console.log("I'm not sure what, but something went worng :(");
+     }
+  
+    }
+//console.log(playRound(playerSelection,computerSelection))
+
+function game(){
+    for(i = 0; i < 5; i++) {
+    const playerSelection = "scissors";
+    const computerSelection = computerPlay();
+    const currentRound = playRound(playerSelection, computerSelection);
+    console.log(currentRound);
+   }
+}
